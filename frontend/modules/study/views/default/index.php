142fe54be990a80315dda43ebf557818c3917d02
root <root@125.76.239.244>
2016-12-14 15:37:55 +0800

    vvvvvvv

--- frontend/modules/study/views/default/index.php
+++ frontend/modules/study/views/default/index.php
@@ -1,5 +1,5 @@
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-    <h1 class="page-header"><i class="fa fa-cog fa-spin"></i>&nbsp;控制台<small id="dright_content">&nbsp;&nbsp;&nbsp;欢迎使用XXX后台管理系统</small></h1>
+    <h1 class="page-header"><i class="fa fa-cog fa-spin"></i>&nbsp;控制台<small id="dright_content">&nbsp;&nbsp;&nbsp;欢迎使用YAN后台管理系统</small></h1>
     <!-- 载入左侧菜单指向的php（或html等）页面内容 -->
     <div id="content">
         <h4>    				
