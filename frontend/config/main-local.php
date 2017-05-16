05c6377845d52816b5bd00028c71aa4851b782ea
ycv <ration@163.com>
2017-05-16 17:24:13 +0800

    Yii2TestCar  2017-05-16 Signed-off-by:ycv <ration@163.com>

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -3,6 +3,7 @@
 $config = [
     'components' => [
         'request' => [
+            ////CSRF跨站请求伪造攻击防范
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'jdSLgl9KJGYXz1uVJ7t8-bXHnztMzICk',
         ],
@@ -14,11 +15,15 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        //配置允许远程调试，显示调试状态栏
+        'allowedIPs' => ['127.0.0.1', '::1']
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        //配置允许远程调试，显示Gii
+        'allowedIPs' => ['127.0.0.1', '::1']
     ];
 }
 
