0b162a9dde2c6621aa68f3c19b66ca6dd384d36f
ycv <ration@163.com>
2017-05-16 18:03:11 +0800

    Yii2TestCar谁谁谁水水水水水水水水所所所事实上事实上啥啥啥4444444444444444444444444444444444444444444444444444444444444  2017-05-16 Signed-off-by:ycv <ration@163.com>

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
@@ -8,10 +9,7 @@ require(__DIR__ . '/../../common/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
 
 $config = yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../common/config/main.php'),
-    require(__DIR__ . '/../../common/config/main-local.php'),
-    require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main-local.php')
+                require(__DIR__ . '/../../common/config/main.php'), require(__DIR__ . '/../../common/config/main-local.php'), require(__DIR__ . '/../config/main.php'), require(__DIR__ . '/../config/main-local.php')
 );
 
 (new yii\web\Application($config))->run();
