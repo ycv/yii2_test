0cc9bd5e0c927603c26a8de4649d18dbf7aa8ceb
yan <yan@yan-PC>
2017-05-23 17:03:50 +0800

    谁谁谁水水水水水水水水升水是三生三世三生三世是

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -13,4 +13,7 @@ $this->title = $name;
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
     </div>
+    <?php if (404 == $exception->statusCode) { ?>
+        <img src="<?= Yii::$app->request->hostInfo ?><?= Yii::getAlias('@web/statics/common/images/') ?>404.jpg" />
+    <?php } ?>
 </div>
