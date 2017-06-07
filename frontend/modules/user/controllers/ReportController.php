9589489dd54e814b3090af4b8d7867747b85c6cc
yan <yan@yan-PC>
2017-06-07 13:54:51 +0800

    嘻嘻嘻

--- frontend/modules/user/controllers/ReportController.php
+++ frontend/modules/user/controllers/ReportController.php
@@ -12,7 +12,7 @@ class ReportController extends BaseUserController {
 
     /**
      * 获取 Top10目录报表数据  
-     * ssscc sssccsssccvv
+     * ssscc sssccssscccc
      */
     public function actionProjectreport() {
         //获取缓存组件
