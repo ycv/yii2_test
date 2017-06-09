72e1ff20355231d1f6db869ffab8bd42b4f63257
yan <yan@yan-PC>
2017-06-09 11:49:43 +0800

    cccccccccccccc

--- frontend/modules/user/views/report/reportlist.php
+++ frontend/modules/user/views/report/reportlist.php
@@ -23,6 +23,7 @@ $this->title = '项目跟踪报表';
     $(document).ready(function () {
         function setReptortDataHead() {
             Hidden_Layer_ON();
+
             $("#myTable05").empty();
             $("#myTable05").removeAttr("class");
             if (!$("#myTable05").hasClass("stripe")) {
@@ -34,59 +35,15 @@ $this->title = '项目跟踪报表';
             if (!$("#myTable05").hasClass("order-column")) {
                 $("#myTable05").addClass("order-column");
             }
-            var listHTML = '<thead>';
-            listHTML += '<tr><th rowspan="2">飒飒xx的</th><th colspan="2">HR Information</th><th colspan="3">Contact</th></tr>';
-            listHTML += '<tr><th>Position</th><th>Salary</th><th>Office</th><th>Extn.</th><th>E-mail</th></tr>';
-            listHTML += '</thead>';
+            var listHTML = '<thead><tr><th rowspan="2">Nawwme</th><th colspan="2">HR Information</th><th colspan="3">Contact</th></tr>';
+            listHTML += '<tr><th>Position</th><th>Salary</th><th>Office</th><th>Extn.</th><th>E-mail</th></tr></thead>';
             listHTML += '<tfoot><tr><th>Name</th><th>Position</th><th>Salary</th><th>Office</th><th>Extn.</th><th>E-mail</th></tr></tfoot>';
             $("#myTable05").html(listHTML);
         }
 
-
-        function setReptortDataBody() {
-            $("#myTable05").append("<tr><td>Bruno Nash</td><td>Software Engineer</td><td>London</td><td>6222</td><td>2011/05/03</td><td>$163,500</td></tr>");
-        }
-
         function getReptortDatab() {
             $('#myTable05').DataTable({
-                bProcessing: true,
-                bServerSide: true,
-                sAjaxSource: basepath + "/user/report/getdemoarray",
-                fnServerData: function (sSource) {
-                    $.ajax({
-                        "dataType": 'json',
-                        "type": "POST",
-                        "url": sSource,
-                        "data": {'reporttype': "zz", '_csrf-frontend': _csrf_frontend},
-                        "success": function (json) {
-                            setReptortDataBody(json);
-                            Hidden_Layer_OFF();
-                        },
-                        error: function () {
-                            console.log("error");
-                        }
-                    });
-                },
-                bFilter: true, //过滤功能
-                bSort: true, //排序功能
-                bInfo: true, //页脚信息
-                bAutoWidth: true, //自动宽度
-                aaSorting: [[2, "desc"]], //从第0列开始，以第2列倒序排列
-                //改变语言
-                "oLanguage": {
-                    "sLengthMenu": "每页显示 _MENU_ 条记录",
-                    "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
-                    "sInfoEmpty": "没有数据",
-                    "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
-                    "oPaginate": {
-                        "sFirst": "首页",
-                        "sPrevious": "前一页",
-                        "sNext": "后一页",
-                        "sLast": "尾页"
-                    },
-                    "sZeroRecords": "抱歉， 没有找到",
-                    "sProcessing": "<img src='" + basepath + "/statics/common/images/loading.gif' />"
-                },
+                "ajax": basepath + "/user/report/getdemoarray",
                 scrollY: "260px",
                 scrollX: true,
                 scrollCollapse: true,
@@ -98,11 +55,13 @@ $this->title = '项目跟踪报表';
                     leftColumns: 1
                 }
             });
-
+            Hidden_Layer_OFF();
         }
+
 
         setReptortDataHead();
         getReptortDatab();
+
     });
 </script>
 
@@ -119,32 +78,7 @@ $this->title = '项目跟踪报表';
 </style>
 
 <div style="height: 420px;width: 100%;">
-    <table id="myTable05" cellpadding="0" cellspacing="0" style="width: 100%;">
-<!--        <thead>
-            <tr>
-                <th rowspan="2">Nawwme</th>
-                <th colspan="2">HR Information</th>
-                <th colspan="3">Contact</th>
-            </tr>
-            <tr>
-                <th>Position</th>
-                <th>Salary</th>
-                <th>Office</th>
-                <th>Extn.</th>
-                <th>E-mail</th>
-            </tr>
-        </thead>
-        <tfoot>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Salary</th>
-                <th>Office</th>
-                <th>Extn.</th>
-                <th>E-mail</th>
-            </tr>
-        </tfoot>-->
-    </table>
+    <table id="myTable05" cellpadding="0" cellspacing="0" style="width: 100%;"></table>
 </div>
 <div class="clear"></div>
 
